71d0a90abc1a551831131e36964934f2bbd528e1
em843 <erinmurphy843@gmail.com>
2022-03-03 16:51:34 -0500

    Add functions.py + dictionaries

--- data.py
+++ data.py
@@ -1,6 +1,26 @@
 
 
-users = {
+
+
+# Voters and their public keys
+public_keys = {
     "grumble": 12345679,
     "goob": 432473298
 }
+
+# Validation 
+validation_numbers = {}
+
+
+#registers
+# name, public keys, validation numbers
+
+# validation numbers generated on sign-in
+
+
+priv_keys = {
+    "grumble": 23432432798,
+    "goob": 2322343
+}
+
+aes_keys = {}
