5e8d63e48856262d530b786f64812efbd6fafcf1
em843 <erinmurphy843@gmail.com>
2022-03-03 17:32:09 -0500

    Work on register function

--- functions.py
+++ functions.py
@@ -13,23 +13,61 @@ candidate and adds one to the tally. After all the votes have been received,
 the CTF publishes the outcome.
 """
 from data import *
+import gmpy2
+from cryptography.hazmat.primitives.asymmetric import rsa
+
+
+
+
+my_e = 65537
+
+
+
+def simple_rsa_encrypt(x, e, n):
+    return gmpy2.powmod(x, e, n)
+
+def simple_rsa_decrypt(y, d, n):
+    return gmpy2.powmod(y, d, n)
+
 
 
 """
 Register to vote
 """
 def register(user):
-    validation_number[user] = get_validation()
-    # Encrypt 
+    # Check if the user asking for a validation number is authentic (signature)
+    if check_signature(): # if signature is valid CHANGE THIS 
+        my_vn = get_validation()
+        validation_number[user] = my_vn
+
+    # Encrypt validation number with user's public key
+    encrypted_vn = simple_rsa_encrypt(my_vn, my_e, public_key[user])
+    print(f"Encrypted validation number: {encrypted_vn}")
+    # Encrypt new AES key with user's public key
+    encrypted_aes = simple_rsa_encrypt(get_aes(), my_e, public_key[user])
+    print(f"Encrypted AES key: {encrypted_aes}")
+
+    # Decrypt validation number with user's priv key
+    decrypted_vn = simple_rsa_decrypt(encrypted_vn, priv_key[user], public_key[user])
+    print(f"Decrypted validation number: {decrypted_vn}")
+    # Decrypt AES key with user's priv key
+    decrypted_aes = simple_rsa_decrypt(encrypted_aes, priv_key[user], public_key[user])
+    print(f"Decrypted AES key: {decrypted_aes}")
     priv_key[user]
-    return
+    return 
+
+
+register("grumble")
+
+def check_signature():
+    return True
 
 
 def get_validation():
     return 12345
 
-def generate_aes():
-    return
+def get_aes():
+    return randint()
 
 
 
