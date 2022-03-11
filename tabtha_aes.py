b412ffdf6aaae4f6d92e917c34717cfb51c3dae6
tabitha-rowland <tabirowland@gmail.com>
2022-03-11 15:28:31 -0500

    aes is comeplete! come steal these functions

--- tabtha_aes.py
+++ tabtha_aes.py
@@ -28,16 +28,37 @@ def int_to_bytes(i):
     i = int(i)
     return i.to_bytes(32, byteorder='big')
 
+#uses all of the above functions & class
+def encryptAES(message, key): #prints out plaintext message, and ciphertext message
+    key = int_to_bytes(key)
+    manager = EncryptionManager(key)
+    plaintexts = [message]
+    ciphertexts = []
+        
+    print("plaintexts: ", plaintexts)
+    for m in plaintexts:
+            ciphertexts.append(manager.updateEncryptor(m))
+    ciphertexts.append(manager.finalizeEncryptor())
+    print("ciphertexts: ", ciphertexts)
+
+    return ciphertexts
+
+def decryptAES(ciphertexts, key):
+    key = int_to_bytes(key)
+    manager = EncryptionManager(key)
+    recoveredtexts = []
+    #expected = [message, b'', b''] #when it passes through the updateEncryptor or updateDecryptor, an empty byte sstring b' ' gets added. dont know how to avoid this.
+    #add message as a parameter or hardcode it in if you want to use this to check
+    
+
+    for c in ciphertexts:
+            recoveredtexts.append(manager.updateDecryptor(c))
+    recoveredtexts.append(manager.finalizeDecryptor())
+    print("recovered: ", recoveredtexts)
 
 
-if __name__ == "__main__":
-
-        #variables for the test
-        message = b"validationNumber and signature"
-        key = 12345678901234567890123456789012
+def allAES(message, key):
         key = int_to_bytes(key)
-
-
         manager = EncryptionManager(key)
         plaintexts = [message]
         ciphertexts = []
@@ -58,24 +79,58 @@ if __name__ == "__main__":
         print("expected: ", expected)
 
 
+if __name__ == "__main__":
+
+        # #variables for the test
+        message = b"validationNumber and signature"
+        key = 12345678901234567890123456789012
+
+        encryptAES(message, key)
+        ciphertexts = encryptAES(message, key)
+        decryptAES(ciphertexts, key)
+
+        allAES(message, key)
+
+
+        # manager = EncryptionManager(key)
+        # plaintexts = [message]
+        # ciphertexts = []
+        # recoveredtexts = []
+        # expected = [message, b'', b''] #when it passes through the updateEncryptor or updateDecryptor, an empty byte sstring b' ' gets added. dont know how to avoid this.
+        
+        # print("plaintexts: ", plaintexts)
+        # for m in plaintexts:
+        #         ciphertexts.append(manager.updateEncryptor(m))
+        # ciphertexts.append(manager.finalizeEncryptor())
+        # print("ciphertexts: ", ciphertexts)
+
+        # for c in ciphertexts:
+        #         recoveredtexts.append(manager.updateDecryptor(c))
+        # recoveredtexts.append(manager.finalizeDecryptor())
+
+        # print("recovered: ", recoveredtexts)
+        # print("expected: ", expected)
+
+
         #things ive tried already to get rid of the b ' 
         #new_recovered = recoveredtexts.decode('utf-8')
 
         #new_recovered = {x.replace("b'", '').replace("'"", '') for x in recoveredtexts}
 
-        # for s in recoveredtexts: #strip whitespaces except space!
+        # for s in recoveredtexts: #strip 
         #         new_recovered = recoveredtexts.replace("[", b'')
 
 
         
         #print("new recovered: ", new_recovered)
 
-        if expected == recoveredtexts:
-                print("[PASS]")
-        else:
-                for r_text, x_text in zip(expected, recoveredtexts):
-                        if r_text!=x_text:
-                                print("Mismatch",r_text,x_text)
-                print("[FAIL]")
+        # if expected == recoveredtexts:
+        #         print("[PASS]")
+        # else:
+        #         for r_text, x_text in zip(expected, recoveredtexts):
+        #                 if r_text!=x_text:
+        #                         print("Mismatch",r_text,x_text)
+        #         print("[FAIL]")
+
 
         
